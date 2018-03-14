ee34bd10dbaa2f8f81dda5faef583ac42510405d
kongtianyi <598515595@qq.com>
2018-03-14 15:50:13 +0800

    修改启动shell，动态配置启动参数。

--- isSpider/start.sh
+++ isSpider/start.sh
@@ -1,1 +1,17 @@
-nohup celery -A tasks -Q beijing_queue worker -c 1 --loglevel=info --hostname=beijing --without-heartbeat >> worker.log 2>&1 &
+IP_PATTERN="^[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$"
+echo "You are starting a downloader worker."
+echo "Please enter this VPS's ip address(ex.114.67.225.0):"
+read ip
+while [[ ! $ip =~ $IP_PATTERN ]];do
+	echo "Input string not match an ip address!"
+	echo "Please enter this VPS's ip address(ex.114.67.225.0):"
+	read ip
+done
+echo "Please enter this VPS's geography address(ex. 北京):"
+read address
+echo -e "{\n\t\"local_ip\": \"$ip\",\n\t\"local_address\": \"$address\"\n}" > ext_conf.json
+echo "Please enter queue's name that this worker will subscribe:"
+read queue_name
+echo "Please enter hostname of this worker:"
+read hostname
+nohup celery -A tasks -Q $queue_name worker -c 1 --loglevel=info --hostname=$hostname --without-heartbeat >> worker.log 2>&1 &
--- isSpider/start2.sh
+++ isSpider/start2.sh
@@ -1,1 +1,2 @@
-nohup celery -A tasks -Q default_queue worker --loglevel=info --hostname=beijing_default --without-heartbeat >> default_queue.log 2>&1 &
+echo "You are starting a worker who handled default_queue's tasks."
+nohup celery -A tasks -Q default_queue worker --loglevel=info --hostname=default --without-heartbeat >> default_queue.log 2>&1 &
