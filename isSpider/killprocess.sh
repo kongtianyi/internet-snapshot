eda132376a7ad34940259ef48fead281ea19651d
kongtianyi <598515595@qq.com>
2018-03-14 18:56:15 +0800

    改了一下killprocess，把读命令行参数改成提示输入参数，避免误操作。

--- isSpider/killprocess.sh
+++ isSpider/killprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROCESS_NAME=$1
-echo $PROCESS_NAME
+echo "Please input the process's name that you want to kill:"
+read PROCESS_NAME
 ID=`ps -ef | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}'`
 echo "$PROCESS_NAME has ids: $ID"
 echo "---------------"
