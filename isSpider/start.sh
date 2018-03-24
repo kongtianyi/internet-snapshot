58b1a03563bf0cdffb046ac6add1af3a4e829426
kongtianyi <598515595@qq.com>
2018-03-24 10:08:03 +0800

    增加pidfile的自动分配

--- isSpider/start.sh
+++ isSpider/start.sh
@@ -5,4 +5,5 @@ read queue_name
 echo "Please enter hostname of this worker(ex. jdcloud):"
 read hostname
 log_file_name="./"${queue_name}".log"
-celery worker -A tasks -Q ${queue_name} -D -c 1 -l info -n ${hostname} -f ${log_file_name} --heartbeat-interval=60
+pid_file_name="./"${queue_name}".pid"
+celery worker -A tasks -Q ${queue_name} -D -c 1 -l info -n ${hostname} -f ${log_file_name} --heartbeat-interval=60 --pidfile=${pid_file_name}
