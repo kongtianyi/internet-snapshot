3c68c4de22a0b07bfdd9b4781ac20c3316e2cacb
kongtianyi <598515595@qq.com>
2018-03-24 10:19:29 +0800

    把celery的time_zone配置去掉，不然定时任务疯狂发。

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -93,7 +93,6 @@ CELERY_BROKER_URL = 'amqp://kongtianyi:kongtianyiderabbitmq@114.67.225.0:5672/kv
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TIMEZONE = 'Asia/Shanghai'
 CELERY_ENABLE_UTC = False
 
 CELERY_ACKS_LATE = True
