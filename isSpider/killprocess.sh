adc44973a39033fbf0cebfa5c896596616fca49a
kongtianyi <598515595@qq.com>
2018-04-04 19:58:42 +0800

    对启动和关闭脚本进行优化

--- isSpider/killprocess.sh
+++ isSpider/killprocess.sh
@@ -1,13 +1,51 @@
 #!/bin/bash
 
-echo "Please input the process's name that you want to kill:"
-read PROCESS_NAME
-ID=`ps -ef | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}'`
-echo "$PROCESS_NAME has ids: $ID"
-echo "---------------"
-for id in $ID
-do
-	echo "killed $id"
-	kill -9 $id
-done
-echo "---------------"
+echo "Please select(or input) the process's name that you want to kill:"
+echo -e "1. celery\t2. geckodriver"
+echo -e "3. firefox\t4. all of the above"
+read select_num
+if [ $select_num != "4" ]
+then
+	case $select_num in
+		1) PROCESS_NAME="celery";;
+		2) PROCESS_NAME="geckodriver";;
+		3) PROCESS_NAME="firefox";;
+	esac
+	ID=`ps -ef | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}'`
+	echo "$PROCESS_NAME has ids: $ID"
+	echo "---------------"
+	for id in $ID
+	do
+		echo "killed $id"
+		kill -9 $id
+	done
+	echo "---------------"
+else
+	ID=`ps -ef | grep celery | grep -v "grep" | awk '{print $2}'`
+	echo "celery has ids: $ID"
+	echo "---------------"
+	for id in $ID
+	do
+		echo "killed $id"
+		kill -9 $id
+	done
+	echo "---------------"
+	ID=`ps -ef | grep geckodriver | grep -v "grep" | awk '{print $2}'`
+	echo "geckodriver has ids: $ID"
+	echo "---------------"
+	for id in $ID
+	do
+		echo "killed $id"
+		kill -9 $id
+	done
+	echo "---------------"
+	ID=`ps -ef | grep firefox | grep -v "grep" | awk '{print $2}'`
+	echo "firefox has ids: $ID"
+	echo "---------------"
+	for id in $ID
+	do
+		echo "killed $id"
+		kill -9 $id
+	done
+	echo "---------------"
+fi
--- isSpider/start.sh
+++ isSpider/start.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-echo "Please enter queue's name that this worker will subscribe(ex. beijing_queue):"
-read queue_name
+echo "Please select(or input) queue's name that this worker will subscribe:"
+echo -e "1. default_queue\t2. parse_queue"
+echo -e "3. beijing_queue\t4. beijing_download_queue"
+echo -e "5. shenzhen_queue\t6. shenzhen_download_queue"
+echo -e "7. chengdu_queue\t8. chengdu_download_queue"
+read select_num
+case $select_num in
+    1) queue_name="default_queue";;
+    2) queue_name="parse_queue";;
+    3) queue_name="beijing_queue";;
+    4) queue_name="beijing_download_queue";;
+    5) queue_name="shenzhen_queue";;
+	6) queue_name="shenzhen_download_queue";;
+	7) queue_name="chengdu_queue";;
+	8) queue_name="chengdu_download_queue";;
+    *) queue_name=${select_num};;
+esac
 echo "Please enter hostname of this worker(ex. jdcloud):"
 read hostname
 log_file_name="./"${queue_name}".log"
