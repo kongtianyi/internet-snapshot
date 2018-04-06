9e1dbdcb39a24754aa3d2e6d828768bb6c9dc362
kongtianyi <598515595@qq.com>
2018-04-06 15:57:19 +0800

    让ssh使用结束后关闭连接，并且加上一个超时的try-except

--- mysite/isadmin/tools/tools.py
+++ mysite/isadmin/tools/tools.py
@@ -5,11 +5,11 @@ import pexpect
 
 def get_ssh_connection(ip, user, password):
     """获取ssh链接"""
-    child = pexpect.spawn("ssh %s@%s" % (user, ip))
-    result = child.expect(["(yes/no)", "(.*?)password:"])
+    child = pexpect.spawn("ssh %s@%s" % (user, ip), timeout=10)
+    result = child.expect(["(yes/no)", "(.*?)password:"], timeout=5)
     if result == 0:
         child.sendline("yes")
-        child.expect("(.*?)password:")
+        child.expect("(.*?)password:", timeout=5)
         child.sendline(password)
     elif result == 1:
         child.sendline(password)
